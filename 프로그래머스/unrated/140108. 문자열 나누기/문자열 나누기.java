b970fa8c458a2b5e92629c57d1594dc5ef25554b
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-05-20 14:51:45 +0900

    [unrated] Title: 문자열 나누기, Time: 0.56 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -2,33 +2,28 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
 
-        return getDivCount(new ArrayList<>(), s).size();
+             char prev = '1';
+        int sameCount = 0, diffCount = 0, answer = 0;
 
-    }
-
-    private List<String> getDivCount(List<String> list, String str) {
-        if (str.length() == 0) {
-            return list;
-        }
-
-        char start = str.charAt(0);
-        int sameCount = 0;
-        int diffCount = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-
-            if (str.charAt(i) == start) sameCount++;
-            else diffCount++;
+        for (char c : s.toCharArray()) {
+            if (prev == '1') {
+                prev = c;
+                sameCount++;
+                answer++;
+            } else if (prev == c) {
+                sameCount++;
+            } else {
+                diffCount++;
+            }
 
             if (sameCount == diffCount) {
-                list.add(str.substring(0, i + 1));
-                return getDivCount(list, str.substring(i + 1));
+                prev = '1';
+                sameCount = 0;
+                diffCount = 0;
             }
         }
+        
+        return answer;
 
-        // sameCount, diffCount 가 다른 상태로 for 문 종료
-        list.add(str);
-
-        return list;
     }
 }
