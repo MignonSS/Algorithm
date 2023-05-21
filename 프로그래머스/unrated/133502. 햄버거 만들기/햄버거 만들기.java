8c52db922e3902642e716fc88b52aeeabf9fa0f6
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-05-21 14:56:40 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.02 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,23 +1,17 @@
 import java.util.*;
 class Solution {
     public int solution(int[] ingredient) {
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
         int answer = 0;
-
-        Stack<Integer> stack = new Stack<>();
-
-        for (int in : ingredient) {
-            stack.push(in);
-
-            if (stack.size() >= 4) {
-
-                if (stack.get(stack.size() - 4) == 1
-                && stack.get(stack.size() - 3) == 2
-                && stack.get(stack.size() - 2) == 3
-                && stack.get(stack.size() - 1) == 1) {
-
-                    answer++;
-                    stack.pop(); stack.pop(); stack.pop(); stack.pop();
-                }
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                    && stack[sp - 2] == 3
+                    && stack[sp - 3] == 2
+                    && stack[sp - 4] == 1) {
+                sp -= 4;
+                answer++;
             }
         }
         return answer;
