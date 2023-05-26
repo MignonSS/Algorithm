80991309dfa0cc160955fbe41a244421d9714843
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-05-26 13:59:44 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 21.22 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,70 +1,30 @@
 import java.util.*;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        // terms 맵으로 변환
-        Map<String, Integer> termsMap = new HashMap<>();
-        for (String term : terms) {
-            String[] arr = term.split(" ");
-            termsMap.put(arr[0], Integer.parseInt(arr[1]));
+      List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> termMap = new HashMap<>();
+        int date = getDate(today);
+
+        for (String s : terms) {
+            String[] term = s.split(" ");
+
+            termMap.put(term[0], Integer.parseInt(term[1]));
         }
-
-        // today : String -> int[]
-        int[] todayArr = getDateArr(today);
-
-        /** 수집일자 + 약관종류 나눈 배열로 만든 후 expired() 를 통해 확인
-         * curPrivacy = String[]{{"2021.05.02", "A"}, ...}
-         */
-
-        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < privacies.length; i++) {
-            String[] curPrivacy = privacies[i].split(" ");
-            if (expired(todayArr, curPrivacy, termsMap)) list.add(i + 1);
-        }
+            String[] privacy = privacies[i].split(" ");
 
-        return list.stream().mapToInt(Integer::intValue).toArray();
+            if (getDate(privacy[0]) + (termMap.get(privacy[1]) * 28) <= date) {
+                answer.add(i + 1);
+            }
+        }
+        return answer.stream().mapToInt(integer -> integer).toArray();
     }
 
-    /** // 년월일 문자를 int[] 배열로 변환
-     * String input: "year.month.date"  (ex. 2023.05.26)
-     * result[0]: year
-     * result[1]: month
-     * result[2]: day
-     */
-    private int[] getDateArr(String dayString) {
-        String[] strArr = dayString.split("\\.");
-
-        return new int[]{
-                Integer.parseInt(strArr[0]),
-                Integer.parseInt(strArr[1]),
-                Integer.parseInt(strArr[2])
-        };
-    }
-
-    private boolean expired(int[] today, String[] privacyAndTermArr, Map<String, Integer> termsMap) {
-        int validTerms = termsMap.get(privacyAndTermArr[1]);
-        int[] collected = getDateArr(privacyAndTermArr[0]);
-        int[] expiredDate = new int[3];
-
-        expiredDate[2] = collected[2] == 1 ? 28 : collected[2] - 1;
-        expiredDate[1] = collected[2] == 1 ? collected[1] + validTerms - 1 : collected[1] + validTerms;
-
-        int toAddYear = 0; 
-        while (expiredDate[1] > 12) {
-            expiredDate[1] -= 12;
-            toAddYear++;
-        }
-        
-        expiredDate[0] = collected[0] + toAddYear;
-
-        // year 비교
-        if (today[0] > expiredDate[0]) return true;
-        if (today[0] < expiredDate[0]) return false;
-
-        // year 동일 -> month 비교
-        if (today[1] > expiredDate[1]) return true;
-        if (today[1] < expiredDate[1]) return false;
-
-        // year, month 동일 -> date 비교
-        return today[2] > expiredDate[2];
+    private int getDate(String today) {
+        String[] date = today.split("\\.");
+        int year = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int day = Integer.parseInt(date[2]);
+        return (year * 12 * 28) + (month * 28) + day;
     }
 }
