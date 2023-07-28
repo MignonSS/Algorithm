9e2810a928389dc38b40d494f0bad797abdf7cbd
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-07-28 19:42:10 +0900

    [unrated] Title: 추억 점수, Time: 3.50 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+    Map<String, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < name.length; i++) {
+        map.put(name[i], yearning[i]);
+    }
+
+    return Arrays.stream(photo)
+            .mapToInt(p -> Arrays.stream(p).mapToInt(n -> map.getOrDefault(n, 0)).sum())
+            .toArray();
+
+    }
+}
