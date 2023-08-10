23194c3f32e3531c6a080902a5bf3f62ebe7e53c
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-08-10 12:11:18 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.29 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,34 +1,30 @@
 class Solution {
     public String solution(int[] numbers, String hand) {
+        StringBuilder answer = new StringBuilder();
+        int curLeft = 10, curRight = 12;
 
-        int curL = 10;
-        int curR = 12;
-
-        StringBuilder sb = new StringBuilder();
-        
-        for (int num : numbers) {
-            if (num == 0) num = 11;
-            if (num % 3 == 1) { // num = 1, 4, 7
-                sb.append("L");
-                curL = num;
-            } else if (num % 3 == 0) {
-                sb.append("R");
-                curR = num;
+        for (int number : numbers) {
+            if (number == 0) number = 11;
+            if (number % 3 == 1) { // number = 1, 4, 7
+                answer.append("L");
+                curLeft = number;
+            } else if (number % 3 == 0) { // number = 3, 6, 9
+                answer.append("R");
+                curRight = number;
             } else {
-                int disFromL = getDistance(curL, num);
-                int disFromR = getDistance(curR, num);
+                int disFromL = getDistance(curLeft, number);
+                int disFromR = getDistance(curRight, number);
 
                 if (disFromL < disFromR || (disFromL == disFromR && hand.equals("left"))) {
-                    sb.append("L");
-                    curL = num;
+                    answer.append("L");
+                    curLeft = number;
                 } else {
-                    sb.append("R");
-                    curR = num;
+                    answer.append("R");
+                    curRight = number;
                 }
             }
         }
-
-        return sb.toString();
+        return answer.toString();
     }
 
     private int getDistance(int from, int to) {
