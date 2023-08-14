21966b5952ac1e5385cf485ea1534087b85e1036
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-08-14 11:20:02 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 17.19 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,30 +1,33 @@
 import java.util.*;
+import java.util.stream.IntStream;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-      List<Integer> answer = new ArrayList<>();
-        Map<String, Integer> termMap = new HashMap<>();
-        int date = getDate(today);
+        Map<String, Integer> termsMap = new HashMap<>();
 
-        for (String s : terms) {
-            String[] term = s.split(" ");
-
-            termMap.put(term[0], Integer.parseInt(term[1]));
+        for (String term : terms) {
+            termsMap.put(term.split(" ")[0], Integer.parseInt(term.split(" ")[1]));
         }
-        for (int i = 0; i < privacies.length; i++) {
-            String[] privacy = privacies[i].split(" ");
 
-            if (getDate(privacy[0]) + (termMap.get(privacy[1]) * 28) <= date) {
-                answer.add(i + 1);
-            }
-        }
-        return answer.stream().mapToInt(integer -> integer).toArray();
+        return IntStream.range(0, privacies.length)
+                        .mapToObj(i -> (privacies[i] + " " + i).split(" "))
+                        .filter(p -> isExpired(today, p, termsMap))
+                        .mapToInt(p -> Integer.parseInt(p[2]) + 1)
+                        .toArray();
     }
 
-    private int getDate(String today) {
-        String[] date = today.split("\\.");
-        int year = Integer.parseInt(date[0]);
-        int month = Integer.parseInt(date[1]);
-        int day = Integer.parseInt(date[2]);
-        return (year * 12 * 28) + (month * 28) + day;
+    private boolean isExpired(String today, String[] privacyArr, Map<String, Integer> termsMap) {
+        int todayToInt = getDateToInt(today, 0);
+        int privacyToInt = getDateToInt(privacyArr[0], termsMap.get(privacyArr[1])) - 1; // 시작일을 포함해야 하기 때문에 결과값에서 -1
+
+        return todayToInt > privacyToInt;
+    }
+
+    private int getDateToInt(String date, int term) {
+        String[] dateArr = date.split("\\.");
+
+        return  Integer.parseInt(dateArr[0]) * 12 * 28
+                + Integer.parseInt(dateArr[1]) * 28
+                + Integer.parseInt(dateArr[2])
+                + term * 28;
     }
 }
