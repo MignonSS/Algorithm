5b16148a6cb12ae07918b7b380a912d71b93dae4
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-08-15 18:02:40 +0900

    [level 1] Title: 신고 결과 받기, Time: 1.50 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,29 +3,27 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         Map<String, Integer> reportedCount = new HashMap<>();
         Map<String, HashSet<String>> myReportIds = new HashMap<>();
-        HashSet<String> banList = new HashSet<>();
-        
+
         Arrays.stream(report)
                 .distinct() // 한 유저가 동일한 아이디를 신고하는 경우 제외
                 .map(str -> str.split(" "))
                 .forEach(arr -> {
-//                    if (arr[0].equals(arr[1])) return;
-                    reportedCount.put(arr[1], reportedCount.getOrDefault(arr[1], 0) + 1);
-                    if (reportedCount.get(arr[1]) >= k) banList.add(arr[1]); 
-                    
+                    reportedCount.put(arr[1], reportedCount.getOrDefault(arr[1], 0) + 1); // 신고당한 횟수 카운트
+
                     HashSet<String> reportedSet = myReportIds.getOrDefault(arr[0], new HashSet<>());
                     reportedSet.add(arr[1]);
-                    myReportIds.put(arr[0], reportedSet);
+                    myReportIds.put(arr[0], reportedSet); // 한 유저(key)가 누구를 신고했는지(value) 맵으로 표현  
                 });
 
+        // id_list 의 각 id마다 해당 id가 신고한 유저 목록(myReportIds) 과 banList를 비교해 메일 받는 횟수 answer 에 기록
         int[] answer = new int[id_list.length];
         for (int i = 0; i < id_list.length; i++) {
             HashSet<String> curReportIds = myReportIds.getOrDefault(id_list[i], new HashSet<>());
-            for (String banId : banList) {
-                if (curReportIds.contains(banId)) answer[i]++;
+            for (String curReportId : curReportIds) {
+                if (reportedCount.getOrDefault(curReportId, 0) >= k) answer[i]++;
             }
         }
-        
+
         return answer;
     }
 }
