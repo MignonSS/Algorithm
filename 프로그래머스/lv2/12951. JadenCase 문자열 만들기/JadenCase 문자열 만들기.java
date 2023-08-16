d09a68b5753a4b25b999cae4851c424870f6f1e6
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-08-16 19:51:40 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 4.04 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,14 +1,24 @@
+import java.util.stream.*;
 class Solution {
-    public String solution(String s) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(Character.toUpperCase(s.charAt(0)));
+//     public String solution(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         sb.append(Character.toUpperCase(s.charAt(0)));
 
-        for (int i = 1; i < s.length(); i++) {
-            char cur = s.charAt(i);
-            if (s.charAt(i-1) == ' ') sb.append(Character.toUpperCase(cur));
-            else sb.append(Character.toLowerCase(cur));
-        }
+//         for (int i = 1; i < s.length(); i++) {
+//             char cur = s.charAt(i);
+//             if (s.charAt(i-1) == ' ') sb.append(Character.toUpperCase(cur));
+//             else sb.append(Character.toLowerCase(cur));
+//         }
 
-        return sb.toString();
+//         return sb.toString();
+//     }
+    
+        public String solution(String s) {
+        char[] arr = s.toLowerCase().toCharArray();
+        
+        return IntStream.range(0, arr.length)
+                .mapToObj(i -> i != 0 && arr[i - 1] != ' ' ? Character.toLowerCase(arr[i]) : Character.toUpperCase(arr[i]))
+                .map(Object::toString)
+                .collect(Collectors.joining());
     }
 }
