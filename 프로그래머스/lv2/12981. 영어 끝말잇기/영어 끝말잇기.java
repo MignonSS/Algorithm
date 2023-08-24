e4cbc70ef4686f196db96d3929408c3655cdb45c
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-08-24 16:48:25 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,30 +1,18 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-        List<String> list = new ArrayList<>();
-
-        int order = -1;
+        Set<String> called = new HashSet<>();
+        char prevLast = ' ';
 
         for (int i = 0; i < words.length; i++) {
-            if (i == 0) {
-                list.add(words[i]);
-                continue;
+            if ((i != 0 && !words[i].startsWith(String.valueOf(prevLast)))
+                    || called.contains(words[i]) || words[i].length() == 1) {
+                return new int[]{(i % n) + 1, (i / n) + 1};
+            } else {
+                called.add(words[i]);
+                prevLast = words[i].charAt(words[i].length() - 1);
             }
-            String prevEnd = words[i - 1].substring(words[i - 1].length() - 1);
-            if (words[i].startsWith(prevEnd) && !list.contains(words[i])) {
-                list.add(words[i]);
-                continue;
-            }
-            order = i + 1;
-            break;
         }
-
-        if (order == -1) return new int[]{0, 0};
-        int[] answer = new int[2];
-
-        answer[1] = (int) Math.ceil(order * 1.0 / n);
-        answer[0] = order % n == 0 ? n : order % n;
-
-        return answer;
+        return new int[]{0, 0};
     }
 }
