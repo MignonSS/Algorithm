eaede16d8834d7a4d90e7aa9d4dce2e9a4c3a742
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-08-25 23:34:59 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -1,13 +1,14 @@
 class Solution
 {
     public int solution(int n, int a, int b) {
-        return getRound(1, a, b);
-    }
+        int answer = 0;
 
-    public int getRound(int round, int a, int b) {
-        if (Math.abs(a - b) == 1 && Math.min(a, b) % 2 != 0) {
-            return round;
-        }
-        return getRound(round + 1, (a + 1) / 2, (b + 1) / 2);
+        while (a != b) {
+            answer++;
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+        } 
+
+        return answer;
     }
 }
