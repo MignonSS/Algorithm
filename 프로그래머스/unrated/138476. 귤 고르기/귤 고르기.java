33f23a8b25141c79c6c32de093c6f55cb0547dcb
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-08-27 10:31:39 +0900

    [unrated] Title: 귤 고르기, Time: 43.50 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -1,24 +1,20 @@
 import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public int solution(int k, int[] tangerine) {
-        if (k == 1) return 1;
+        Map<Integer, Integer> map = new HashMap<>();
 
-        int[] count = new int[10_000_001];
-        int total = k;
-
-        for (int i = 0; i < tangerine.length; i++) {
-            count[tangerine[i]]++;
-            if (count[tangerine[i]] == k) return 1;
-        }
-
-        Arrays.sort(count);
-
-        int answer = 0;
-        for (int i = 0; total > 0; i++) {
-            total -= count[10000000 - i];
-            answer++;
+        for (int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
         }
         
-        return answer;
+        List<Integer> list = map.values().stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+
+        for (int i = 0; i < list.size(); i++) {
+            k -= list.get(i);
+            if (k <= 0) return (i + 1);
+        }
+        return list.size();
     }
 }
