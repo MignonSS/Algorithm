ab9b06dfafc2ea96086b18ccb59ab5b7cbb15dd6
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-08-29 19:10:11 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 155.31 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,22 +1,16 @@
 import java.util.*;
 class Solution {
     public int solution(int[] elements) {
-
-        Set<Integer> sums = new HashSet<>();
-        int start = 0;
+        Set<Integer> set = new HashSet<>();
 
         for (int i = 0; i < elements.length; i++) {
-            int n = 1;
-            int idx = i;
             int sum = 0;
-
-            while (n <= elements.length) {
-                sum += elements[idx++];
-                sums.add(sum);
-                if (idx >= elements.length) idx = 0;
-                n++;
+            for (int j = 0; j < elements.length; j++) {
+                sum += elements[(i + j) % elements.length];
+                set.add(sum);
             }
         }
-        return sums.size();
+
+        return set.size();
     }
 }
