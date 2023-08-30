6011e2ecd4349c7f0d912a73ab0a4f86bd0167ad
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-08-30 19:35:23 +0900

    [level 2] Title: H-Index, Time: 0.32 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -1,23 +1,12 @@
 import java.util.Arrays;
 class Solution {
     public int solution(int[] citations) {
-        int n = citations.length;
-        int min = Integer.MAX_VALUE;
-        int thesisCount = 0;
-
         Arrays.sort(citations);
-
-        // 오름차순 정렬 후 마지막 요소부터 체크
+        int h = 0;
+        
         for (int i = citations.length - 1; i >= 0; i--) {
-            thesisCount++;
-            int curCitation = citations[i];
-            min = Math.min(curCitation, min);
-
-            if (thesisCount > min) break;
+            if (citations.length - i <= citations[i]) h++;
         }
-
-        if (thesisCount > min) thesisCount--;
-
-        return thesisCount;
+        return h;
     }
 }
