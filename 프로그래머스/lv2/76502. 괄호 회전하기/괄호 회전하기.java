92630e1c1c9c8767929a41864bc3e40e32419a8a
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-09-01 13:14:46 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.16 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,45 +1,32 @@
 import java.util.*;
 class Solution {
+    private static Map<Character, Character> pair = Map.of(')', '(', '}', '{', ']', '[');
+
     public int solution(String s) {
         int count = 0;
+        StringBuilder sb = new StringBuilder(s);
 
         for (int i = 0; i < s.length(); i++) {
-            s = rotationBracket(s);
-            if (isProper(s)) {
-                count++;
-            }
+            s = sb.append(s.charAt(0)).deleteCharAt(0).toString();
+            if (isProper(s)) count++;
         }
-
         return count;
     }
 
-    private String rotationBracket(String s) {
-        return new StringBuilder(s).append(s.charAt(0)).deleteCharAt(0).toString();
-    }
-
+    // 현재 문자열이 올바른 괄호 문자열인지 검사
     private boolean isProper(String s) {
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[' || stack.isEmpty()) {
-                stack.push(s.charAt(i));
-            } else {
-                switch (s.charAt(i)) {
-                    case ')':
-                        checkBracket(stack, '(');
-                        break;
-                    case '}':
-                        checkBracket(stack, '{');
-                        break;
-                    case ']':
-                        checkBracket(stack, '[');
-                        break;
-                }
-            }
+            char curC = s.charAt(i);
+
+            if (!pair.containsKey(curC) || stack.isEmpty()) stack.push(curC);
+            else checkBracket(stack, pair.get(curC));
         }
         return stack.isEmpty();
     }
 
+    // 닫는 괄호가 올바른 위치인지 검사 
     private void checkBracket(Stack<Character> stack, char c) {
         if (stack.peek() == c) stack.pop();
     }
