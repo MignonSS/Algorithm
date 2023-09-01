d87737eb1a52c1a1dcf778462063002384d09802
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-09-01 13:51:16 +0900

    [level 2] Title: [1차] 캐시, Time: 2.08 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,23 +1,23 @@
 import java.util.*;
 class Solution {
-    public int solution(int cacheSize, String[] cities) {
-        Queue<String> cache = new LinkedList<>();
-        int time = 0;
+public int solution(int cacheSize, String[] cities) {
+    Queue<String> cache = new LinkedList<>();
+    int time = 0;
 
-        for (String city : cities) {
-            city = city.toUpperCase();
-            if (cacheSize == 0) {
-                time += 5;
-            } else if (cache.contains(city)) {
-                cache.remove(city);
-                time += 1;
-            } else {
-                if (cache.size() == cacheSize) cache.poll();
-                time += 5;
-            }
-            cache.add(city);
+    if (cacheSize == 0) return cities.length * 5; // 예외 case
+
+    for (String city : cities) {
+        city = city.toUpperCase();
+
+        if (cache.contains(city)) {
+            cache.remove(city);
+            time += 1;
+        } else {
+            if (cache.size() >= cacheSize) cache.poll();
+            time += 5;
         }
-
-        return time;
+        cache.add(city);
     }
+    return time;
+}
 }
