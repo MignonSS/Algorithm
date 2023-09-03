df2d3511ea91a3a43e1ea81d02bbedf46d607b04
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-09-03 13:56:44 +0900

    [level 2] Title: 프로세스, Time: 0.90 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.java
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -1,32 +1,27 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] priorities, int location) {
-        Map<Integer, Integer> map = new HashMap<>();
-        Queue<Integer> q = new LinkedList<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+public int solution(int[] priorities, int location) {
+    Map<Integer, Integer> map = new HashMap<>();
+    Queue<Integer> q = new LinkedList<>();
+    PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
-        for (int i = 0; i < priorities.length; i++) {
-            map.put(i, priorities[i]);
-            q.offer(i);
-            pq.offer(priorities[i]);
-        }
-
-        while (true) {
-            int curLocation = q.poll();
-
-            if (map.get(curLocation) != pq.peek()) {
-                q.offer(curLocation);
-                continue;
-            }
-
-            pq.poll();
-
-            if (curLocation == location) {
-                break;
-            }
-        }
-
-        return priorities.length - pq.size();
+    for (int i = 0; i < priorities.length; i++) {
+        map.put(i, priorities[i]);
+        q.offer(i);
+        pq.offer(priorities[i]);
     }
+
+    while (!q.isEmpty()) {
+        int curLocation = q.poll();
+
+        if (map.get(curLocation) != pq.peek()) {
+            q.offer(curLocation);
+        } else {
+            pq.poll();
+            if (curLocation == location) break;
+        }
+    }
+    return priorities.length - pq.size();
+    }   
 }
