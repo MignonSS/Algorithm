3cccdeaf655006b7feb20059377ffa064c7540fa
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-09-03 19:31:10 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 2.14 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/README.md
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/README.md
@@ -1,6 +1,6 @@
 # [level 2] [1차] 뉴스 클러스터링 - 17677 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677#) 
 
 ### 성능 요약
 
--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,67 +1,37 @@
 import java.util.*;
 
 class Solution {
-        public int solution(String str1, String str2) {
-		str1 = str1.toUpperCase(); // 모두 대문자로 
-		str2 = str2.toUpperCase();
-		
-		ArrayList<String> list1 = new ArrayList<>();
-		ArrayList<String> list2 = new ArrayList<>();
+    public int solution(String str1, String str2) {
+        if (str1.length() == 0 && str2.length() == 0) return 65536;
+        if (str1.length() == 0 || str2.length() == 0) return 0;
 
-		ArrayList<String> union = new ArrayList<>();
-		ArrayList<String> intersection = new ArrayList<>();
+        // 다중집합 만들기
+        Map<String, Integer> map1 = getMultiSet(str1);
+        Map<String, Integer> map2 = getMultiSet(str2);
 
-		// str1 다중 집합 만들기 
-		for(int i = 0; i < str1.length()-1; i++) {
-			char a = str1.charAt(i);
-			char b = str1.charAt(i+1);
+        // 교집합, 합집합
+        int intersectionSize = map1.keySet().stream()
+                .filter(map2::containsKey)
+                .mapToInt(str -> Math.min(map1.get(str), map2.get(str))).sum();
+        int unionSize = map1.values().stream().mapToInt(Integer::intValue).sum()
+                + map2.values().stream().mapToInt(Integer::intValue).sum()
+                - intersectionSize;
 
-			// 문자만 가진 경우만 추가 
-			if(Character.isLetter(a) && Character.isLetter(b)) {
-				String str = Character.toString(a) + Character.toString(b);
-				list1.add(str);
-			}
-		}
+        return (int) ((intersectionSize / (double) unionSize) * 65536);
+    }
 
-		// str2 다중 집합 만들기 
-		for(int i = 0; i < str2.length()-1; i++) {
-			char a = str2.charAt(i);
-			char b = str2.charAt(i+1);
+    private Map<String, Integer> getMultiSet(String str) {
+        Map<String, Integer> map = new HashMap<>();
+        str = str.toUpperCase();
 
-			// 문자만 가진 경우만 추가 
-			if(Character.isLetter(a) && Character.isLetter(b)) {
-				list2.add(Character.toString(a) + Character.toString(b));
-			}
+        for (int i = 0; i < str.length() - 1; i++) {
+            String subStr = str.substring(i, i + 2).replaceAll("[^A-Z]", "");
+
+            if (subStr.length() == 2) {
+                map.put(subStr, map.getOrDefault(subStr, 0) + 1);
+            }
         }
 
-		// 중복 원소 처리를 위해 두 집합 정렬
-		Collections.sort(list1);
-		Collections.sort(list2);
-		
-		// 교집합 구하기 
-		for(String s : list1) {
-			if(list2.remove(s)) { // 집합1에 집합2가 포함된다면 삭제후, 교집합에 추가 
-				intersection.add(s);
-			}
-			union.add(s);
-		}
-		
-		// 합집합 구하기 
-		for(String s : list2) { // 교집합에서 제외된 것 뺀 나머지 합집합에 추가 
-			union.add(s);
-		}
-        
-		// 자카드 유사도 구하기 
-		double a = intersection.size();
-		double b = union.size();
-
-   		double jakard = 0;
-	
-		if(union.size() == 0)
-			jakard = 1;	// 공집합일 경우 1
-		else
-			jakard = (double) intersection.size() / (double) union.size();
-
-		return (int) (jakard * 65536);
+        return map;
     }
 }
