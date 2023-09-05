ed6a1eeaa0404709c2ab8515b2de0796c414fc56
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-09-05 17:11:32 +0900

    [level 2] Title: 튜플, Time: 2.83 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -1,23 +1,25 @@
 import java.util.*;
-import java.util.stream.Collectors;
+
 class Solution {
     public int[] solution(String s) {
         String[] strings = s.substring(2, s.length() - 2).split("\\},\\{");
         Arrays.sort(strings, Comparator.comparingInt(String::length));
 
-        List<String> answer = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+        int[] answer = new int[strings.length];
+        int idx = 0;
 
         for (String str : strings) {
             String[] split = str.split(",");
 
             for (String str2 : split) {
-                if (!answer.contains(str2)) {
-                    answer.add(str2);
-                    break;
+                if (!set.contains(str2)) {
+                    answer[idx++] = Integer.parseInt(str2);
+                    set.add(str2);
                 }
             }
         }
 
-        return answer.stream().mapToInt(Integer::parseInt).toArray();
+        return answer;
     }
 }
