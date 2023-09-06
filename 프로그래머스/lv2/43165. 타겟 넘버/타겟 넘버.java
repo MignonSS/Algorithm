129b55bd91afd14b65dcb1cb57870a6c46cb0672
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-09-06 20:52:51 +0900

    [level 2] Title: 타겟 넘버, Time: 0.65 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,6 @@
 class Solution {
-    static int[] mark;
-    static int answer;
-
-    public void dfs(int depth, int[] numbers, int target) {
-        if (depth == numbers.length) {
-            int temp = 0;
-            for (int i = 0; i < numbers.length; i++) {
-                temp += mark[i] * numbers[i];
-            }
-            if (temp == target) answer++;
-        } else {
-            mark[depth] = 1;
-            dfs(depth + 1, numbers, target);
-            mark[depth] = -1;
-            dfs(depth + 1, numbers, target);
-        }
-    }
-
     public int solution(int[] numbers, int target) {
-        mark = new int[numbers.length];
-        dfs(0, numbers, target);
+        int answer = 0;
         return answer;
     }
 }
