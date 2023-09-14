b1ed6a66725a381c347b98cd0fd53a54dda04896
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-09-15 01:25:29 +0900

    [level 2] Title: [3차] n진수 게임, Time: 2.68 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        char[] arr = new char[t * m];
+        StringBuilder answer = new StringBuilder();
+
+        int temp = 0;
+        String tempStr = "";
+        int digitNum = 0;
+        
+        for (int i = 0, j = 0; i < arr.length; i++) {
+            if (digitNum == 0) {
+                tempStr = Integer.toString(temp, n).toUpperCase();
+            }
+            
+            arr[i] = tempStr.charAt(digitNum++);
+
+            if (digitNum == tempStr.length()) {
+                temp++;
+                digitNum = 0;
+            }
+
+            if ((i + 1) % m == (m == p ? 0 : p)) {
+                answer.append(arr[i]);
+            }
+        }
+        
+        return answer.toString();
+    }
+}
