1019e5c9a72b9ee36ec7c200bc01b65760267fcb
Minhyeong Song <124571603+MignonSS@users.noreply.github.com>
2023-11-13 12:22:44 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,24 +1,11 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int[] answer = new int[2];
-
-        int gcd = gcd(denom1, denom2);
-        System.out.println("gcd : " + gcd);
-        int lcm = denom1 * denom2 / gcd;
-        System.out.println("lcm : " + lcm);
-
-        int share1 = lcm / denom1;
-        int share2 = lcm / denom2;
-        System.out.println("share1 : " + share1 + ", share2 : " + share2);
-
-        int numer3 = numer1 * share1 + numer2 * share2;
-        int denom3 = lcm;
-
-        int gcd2 = gcd(numer3, denom3);
-        answer[0] = numer3 / gcd2;
-        answer[1] = denom3 / gcd2;
+        int denom3 = denom1 * denom2;
+        int numer3 = numer1 * denom2 + numer2 * denom1;
         
-        return answer;
+        int gcd = gcd(denom3, numer3);
+        
+        return new int[]{numer3 / gcd, denom3 / gcd};
     }
     
     public int gcd(int a, int b) {
